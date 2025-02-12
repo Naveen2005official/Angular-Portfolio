04b0a2c2953599624bf4a827d5f6e385d35f8676
Naveen7105official <snk2022it@gmail.com>
2025-02-12 23:12:30 +0530

    Description

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { BgAnimationComponent } from '../bg-animation/bg-animation.component';
+import { DescriptionComponent } from '../description/description.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, BgAnimationComponent],
+  imports: [NavbarComponent, BgAnimationComponent, DescriptionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
