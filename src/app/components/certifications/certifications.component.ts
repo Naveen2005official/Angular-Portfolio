f7b7cd324019fa0e1e495c4ac0674ae53198c158
Naveen7105official <snk2022it@gmail.com>
2025-02-16 21:37:02 +0530

    Techno

--- src/app/components/certifications/certifications.component.ts
+++ src/app/components/certifications/certifications.component.ts
@@ -13,12 +13,14 @@ export class CertificationsComponent {
     {
       image: '../../images/aws.png',
       title: 'AWS Certified Cloud Practitioner CLF-CO2',
-      source: 'https://www.credly.com/badges/fa5822f6-c0ab-485f-95e8-33418f181f88/linked_in_profile'
+      source: 'https://www.credly.com/badges/fa5822f6-c0ab-485f-95e8-33418f181f88/linked_in_profile',
+      description: 'AWS Cloud Practitioner Certification covers the AWS Services and their usages.'
     },
     {
       image: '../../images/java.png',
       title: 'Core Java - Coursera (LearnQuest)',
-      source: 'https://www.coursera.org/account/accomplishments/specialization/09WOY0K65L73'
+      source: 'https://www.coursera.org/account/accomplishments/specialization/09WOY0K65L73',
+      description: 'Core Java includes basic concepts and it also covers OOP concepts.'
     }
   ];
 }
